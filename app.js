6a5a6ca5835316d96117c22dbf714dc81286ba48
kordldurham <66217662+kordldurham@users.noreply.github.com>
2020-08-21 06:40:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+window.addEventListener(`load`, () => {
+    let long;
+    let lat;
+    let temperatrureSection = document.querySelector(`.degreesection`)
+    let temperatureDescription = document.querySelector(`.temperature-description`);
+    let temperatureDegree = document.querySelector(`.temperature-degree`);
+    let locationTimezone = document.querySelector(`.timezone`);
+    let temperatrureUnit = document.querySelector(`.temperature-unit`);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
+            let api = `${proxy}api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=c6e1c6a1b1d5c50aa30efa50ff543300`;
+            //
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data)
+                    const apiTemperature = data.current.temp;
+                    const { description } = data.current.weather[0];
+                    const apiTimezone = data.timezone;
+                    temperatureDegree.textContent = apiTemperature;
+                    temperatureDescription.textContent = description.toUpperCase();
+                    locationTimezone.textContent = apiTimezone;
+                    setIcons(description, document.querySelector(`.icon-display`));
+                    temperatrureSection.addEventListener(`click`, () => {
+                        if (temperatrureUnit.textContent === "C") {
+                            api = `${proxy}api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=c6e1c6a1b1d5c50aa30efa50ff543300`;
+                            fetch(api)
+                                .then(response => {
+                                    return response.json();
+                                })
+                                .then(newdata => {
+                                    let tempdata = newdata.current.temp;
+                                    temperatureDegree.textContent = tempdata;
+                                    temperatrureUnit.textContent = "F";
+                                })
+                        } else {
+                            api = `${proxy}api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=c6e1c6a1b1d5c50aa30efa50ff543300`;
+                            fetch(api)
+                                .then(response => {
+                                    return response.json();
+                                })
+                                .then(newdata => {
+                                    let tempdata = newdata.current.temp;
+                                    temperatureDegree.textContent = tempdata;
+                                    temperatrureUnit.textContent = "C";
+                                })
+                        }
+                    })
+                })
+        })
+
+        function setIcons(icon, iconID) {
+            let skycons = new Skycons({ "color": "#22ff55" });
+            const currentIcon = icon.replace(/ /g, "_").toUpperCase();
+            skycons.play();
+            return skycons.set(iconID, skycons[currentIcon]);
+        }
+    }
+});
